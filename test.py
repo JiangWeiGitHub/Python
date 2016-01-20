adea3c23714a1629fc412f08f91e0f0c3a9c8932
JiangWei <wei.jiang@winsuntech.cn>
2016-01-20 18:14:55 +0800

    modified

--- test.py
+++ test.py
@@ -2,4 +2,11 @@
 # -*- coding: utf-8 -*-
 
 print "hello,    world!     "
+
 print '\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8')
+
+print "Your Score is: %03d, %s!" % (99,'Tom')
+
+names = ['Michael', 'Bob', 'Tracy']
+for name in names:
+    print name
