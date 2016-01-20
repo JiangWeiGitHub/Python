4dce39e131b21c584b6e9296e88ad51f053fb3cb
JiangWei <wei.jiang@winsuntech.cn>
2016-01-20 18:18:43 +0800

    modified

--- test.py
+++ test.py
@@ -7,6 +7,17 @@ print '\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8')
 
 print "Your Score is: %03d, %s!" % (99,'Tom')
 
+print "##########################"
+print "list test:"
 names = ['Michael', 'Bob', 'Tracy']
 for name in names:
     print name
+
+print "##########################"
+print "range test:"
+sum = 0
+for x in range(101):
+    sum = sum + x
+print sum
+
+
