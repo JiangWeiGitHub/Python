cf02beb5eeb5af30f80d5bc985a75da14447210c
Jiang Wei <wei.jiang@winsuntech.cn>
2016-01-25 05:32:31 -0500

    mixed map & reduce.

--- test.py
+++ test.py
@@ -134,5 +134,24 @@ def add(x, y):
 result = reduce(add, [1, 3, 5, 7, 9])
 print result
 
+print "##########################"
+print "map & reduce test:"
+print "def str2int(s):"
+print "    def fn(x, y):"
+print "        return x * 10 + y"
+print "    def char2num(s):"
+print "        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]"
+print "    return reduce(fn, map(char2num, s))"
+print "method: str2int('5211314')"
+print "result:"
+def str2int(s):
+    def fn(x, y):
+        return x * 10 + y
+    def char2num(s):
+        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+    return reduce(fn, map(char2num, s))
+result = str2int('5211314')
+print result
+
 
 
