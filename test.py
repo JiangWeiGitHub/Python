cbc14ae510a56fd446ba0d5e107cb16bf3607557
Jiang Wei <wei.jiang@winsuntech.cn>
2016-01-27 00:31:32 -0500

    added sorted.

--- test.py
+++ test.py
@@ -166,6 +166,18 @@ result = filter(is_odd, [1, 2, 4, 5, 6, 9, 10, 15])
 print "result:"
 print result
 
+print "##########################"
+print "sorted test:"
+def cmp_ignore_case(s1, s2):
+    u1 = s1.upper()
+    u2 = s2.upper()
+    if u1 < u2:
+        return -1
+    if u1 > u2:
+        return 1
+    return 0
+result = sorted(['bob', 'about', 'Zoo', 'Credit'], cmp_ignore_case)
+print result
 
 
 
