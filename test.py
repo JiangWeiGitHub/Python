4553d24ddf9ea937d65a24c46b6ba671f1632283
JiangWei <wei.jiang@winsuntech.cn>
2016-01-30 17:58:41 +0800

    modified a little things.

--- test.py
+++ test.py
@@ -144,7 +144,7 @@ print "        return x * 10 + y"
 print "    def char2num(s):"
 print "        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]"
 print "    return reduce(fn, map(char2num, s))"
-print "method: str2int('5211314')"
+print "str2int('5211314')"
 print "result:"
 def str2int(s):
     def fn(x, y):
